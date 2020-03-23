093e25c0775f19dda4a3188c33016a2cbea9d176
Shaharyar Shamshi <shaharyarshamshi@gmail.com>
2020-03-23 12:00:01 +0530

    enviroment url changed

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient , HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
 
 @Injectable()
@@ -8,13 +7,9 @@ export class AppService {
 
   constructor(private http: HttpClient ) { }
   getIndiaData(): any {                                                                                             
-    
-    const url = `${environment.url}`;
     return this.http.get('https://mysterious-sands-32124.herokuapp.com/getData');
   }
   getHistoricalData(): any {                                                                                             
-    
-    const url = `${environment.url}`;
     return this.http.get('https://mysterious-sands-32124.herokuapp.com/getHistoricalData');
   }
 }
